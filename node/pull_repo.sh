9aeba863b5953849edb803ba938290b38b8e20a1
theverything <j3ffhorn@gmail.com>
2014-05-17 19:41:59 -0700

    update pull repo

--- node/pull_repo.sh
+++ node/pull_repo.sh
@@ -1,8 +1,5 @@
-touch ~/.netrc
-echo "machine <%MACHINE> login <%LOGIN> password <%PASSWORD> protocol https" > ~/.netrc
-
 git clone <%REPO> /home/app
 
-cd /home/app && npm install
+cd /home/app && npm install && gulp build
 
-cd /home/app && node --harmony gulpfile
+cd /home/app && npm start
